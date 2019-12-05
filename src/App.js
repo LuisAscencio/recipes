987cd7d3dfc4868333e0cd8c22b9ed56847294f6
Luis Ascencio <luismiguelascencio@gmail.com>
2019-12-05 17:47:03 -0500

    added get request to obtain recipes from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const APP_ID = `2b747fc4`;
   const APP_KEY = `d4e5d53d398f73289e4ed5fca4d38690	`;
-  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    ).then(response => {
+      response.json().then(data => {
+        console.log(data);
+      });
+    });
+  };
 
   return (
-    <div className="App">
+    <div className="jumbotron jumbotron-fluid">
       <h1>Recipes</h1>
+      <form>
+        <div className="form-row">
+          <div className="col">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="search recipe"
+            />
+          </div>
+          <button type="button" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
