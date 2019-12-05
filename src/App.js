e11fc36e2f11425f20c914a0b6bc593cdb2e76e1
Luis Ascencio <luismiguelascencio@gmail.com>
2019-12-05 17:07:06 -0500

    added API ID and Key

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const APP_ID = `2b747fc4`;
+  const APP_KEY = `d4e5d53d398f73289e4ed5fca4d38690	`;
+  const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Recipes</h1>
     </div>
   );
 }
