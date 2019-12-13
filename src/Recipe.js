db57e9d838b9c1f40f282a00ba051db9a5792150
Luis Ascencio <luismiguelascencio@gmail.com>
2019-12-13 13:08:56 -0500

    styled recipes with bootstrap cards

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,11 +2,15 @@ import React from "react";
 
 const Recipe = ({ title, calories, image }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-
-      <img src={image} alt="" />
-      <p>Calories: {calories}</p>
+    <div className="card" style={{ width: "18rem" }}>
+      <img className="card-img-top" src={image} />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">Calories: {Math.trunc(calories)}</p>
+        <a href="#" className="btn btn-primary">
+          Get Recipe
+        </a>
+      </div>
     </div>
   );
 };
