959750b7b3e26a125dad063e6bae8d7a316d2165
Luis Ascencio <luismiguelascencio@gmail.com>
2019-12-17 12:54:10 -0500

    added get ingredientes button

--- src/RecipeCard.js
+++ src/RecipeCard.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const RecipeCard = ({ title, calories, image, ingredients }) => {
+  const [getRecipeButton, setRecipeButton] = useState(false);
+
+  const updateRecipeButton = e => {
+    setRecipeButton(!getRecipeButton);
+  };
+
   return (
     <div className="row">
       <div className="col-sm-6">
         <div className="card" style={{ width: "30em" }}>
-          <img className="card-img-top" src={image} />
+          <img className="card-img-top" alt="" src={image} />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <ol>
-              {ingredients.map(ingredient => (
-                <li>{ingredient.text}.</li>
-              ))}
-            </ol>
+            {getRecipeButton ? (
+              <ol>
+                {ingredients.map((ingredient, i) => (
+                  <li key={ingredients.length / i - 3}>{ingredient.text}.</li>
+                ))}
+              </ol>
+            ) : null}
+
             <p className="card-text">Calories: {Math.trunc(calories)}</p>
-            <button type="submit" className="btn btn-danger">
+            <button
+              onClick={updateRecipeButton}
+              type="button"
+              className="btn btn-danger"
+            >
               Get Ingredients
             </button>
           </div>
