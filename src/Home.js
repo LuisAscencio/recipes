21887b5a9d831765956b4754a554c3a334116eda
Luis Ascencio <luismiguelascencio@gmail.com>
2019-12-18 09:36:34 -0500

    added navbar

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,64 @@
 import React from "react";
-import MdPizza from "react-ionicons/lib/MdPizza";
+import { FaHamburger } from "react-icons/fa";
 const Home = () => {
   return (
-    <div className="home">
-      <div className="groupButtons">
-        <MdPizza shake={true} fontSize="400px" color="white" />
-        <button className="homeButtons">Recipes Vault</button>
-        <button className="homeButtons2">My recipes</button>
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <FaHamburger size="20px" color="#cb444a" />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Features
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+              >
+                Disabled
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="home">
+        <br />
+        <br />
+        <div className="groupButtons">
+          <FaHamburger size="200px" color="white" />
+        </div>
+        <br />
+        <h2 className="groupButtons">Recipes</h2>
+        <canvas width={window.innerWidth} height={window.innerHeight} />
       </div>
-      <canvas width={window.innerWidth} height={window.innerHeight} />
     </div>
   );
 };
